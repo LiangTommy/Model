138a0941b55e70416733f41240a0690379c907c9
LiangTommy <toliang@xogrp.com>
2015-03-06 10:16:28 +0800

    add file fragmentshow

---
+++ src/com/model/FragmentShow.java
@@ -0,0 +1,18 @@
+package com.model;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+
+public class FragmentShow {
+
+	protected DialogFragment mFragment;
+	
+	protected Fragment fragment;
+	
+	public void debugFragmentShow() {
+		
+		
+		
+	}
+	
+}
