3798dc661e6a557346ba91e73104827e2499e816
LiangTommy <toliang@xogrp.com>
2015-03-20 10:55:36 +0800

    refresh the code

--- src/com/model/SoundActivity.java
+++ src/com/model/SoundActivity.java
@@ -11,4 +11,12 @@ public class SoundActivity {
 		
 	}
 	
+	public void reStart() {
+		int i=0;
+		int c=3;
+		System.out.print("1"+i+i);
+		System.out.print("1"+c);
+		System.out.print("1");
+	}
+	
 }
