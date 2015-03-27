c948869374ee4387ec43845bfdb8bdd022b1d68e
LiangTommy <toliang@xogrp.com>
2015-03-27 10:55:45 +0800

    201503271055

--- src/com/utils/ImageGroupChekout.java
+++ src/com/utils/ImageGroupChekout.java
@@ -9,5 +9,18 @@ public class ImageGroupChekout {
 	 * -library-for-android/ Github: https://github.com/facebook/fresco
 	 */
 
-	 
+	 /**
+	  * （工作态度、表现及业务能力等）
+(Work attitude, performance and Business abilities, etc.) 
+During this time, first, he fixed some crash issues in Planner and TB, but only 1-2 bugs per day. The performance is not good enough. Second, in our team, senior developers need to help junior developers and interns to grow up, but he didn’t play his role.
+So I think he is not consistent with the standard of senior developer in our company.
+主管Department Leader（签字 Sign）： 
+2015年3月27 日
+（工作态度、表现及业务能力等）
+(Work attitude, performance and Business abilities, etc.) 
+According to my observation, Tommy is not that good at English, and he is kind of slow in understanding things, which is kind of a big disadvantage for a senior developer. And he is too quiet for a senior developer.
+
+I think he doesn’t meet the requirement of a senior developer in our company, and he will have a better opportunity in other companies.
+
+	  */
 }
