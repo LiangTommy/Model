5acb69315f79d975896f5b22b351a574ce7fb37d
LiangTommy <toliang@xogrp.com>
2015-03-27 10:36:09 +0800

    develop facebook image

---
+++ src/com/utils/ImageGroupChekout.java
@@ -0,0 +1,13 @@
+package com.utils;
+
+public class ImageGroupChekout {
+
+	/*
+	 * https://github.com/facebook/fresco Hi guys, Announcement:
+	 * https://code.facebook
+	 * .com/posts/366199913563917/introducing-fresco-a-new-image
+	 * -library-for-android/ Github: https://github.com/facebook/fresco
+	 */
+
+	 
+}
